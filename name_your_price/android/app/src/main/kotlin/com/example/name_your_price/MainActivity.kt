5be47d01ee164728c2d618bcc612662105f86160
tuanpm <phamminhtuan1999@gmail.com>
2022-06-10 16:51:34 +0700

    change folder

--- name_your_price/android/app/src/main/kotlin/com/example/name_your_price/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.name_your_price
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- name_your_price/test/smoke_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:name_your_price/main.dart';
-
-void main() {
-  testWidgets('smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(const MyApp());
-  });
-}
